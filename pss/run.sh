7f3983566b3f45ca11eb87bb3310238fc211a7ce
Shamnas <shamnastv@gmail.com>
2020-12-20 23:04:45 +0530

    updated hyper parameters

--- pss/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --job-name=pss # Job name
-#SBATCH --ntasks=4 # Run on a single CPU
-#SBATCH --time=23:50:00 # Time limit hrs:min:sec
-#SBATCH --output=test_job%j.out # Standard output and error log
-#SBATCH --gres=gpu:1
-#SBATCH --partition=cl1_48h-1G
-
-python3 main.py --hidden_dim 50 --lr .0008 --batch_size 32 --dropout .3 --num_layers 1 --beta .5
