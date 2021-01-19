ef79bcededb2f6362651dfb54014e77ebbd395c6
Shamnas <shamnastv@gmail.com>
2021-01-19 23:49:35 +0530

    update

--- RepWalk/model_cpt.py
+++ RepWalk/model_cpt.py
@@ -71,38 +71,38 @@ class RepWalk(nn.Module):
     def __init__(self, embedding_matrix, args):
         super(RepWalk, self).__init__()
         ''' embedding layer '''
-        # self.word_embedding = nn.Embedding.from_pretrained(torch.tensor(embedding_matrix, dtype=torch.float))
-        # self.pos_embedding = nn.Embedding(len(args.tokenizer.vocab['pos']), args.pos_dim, padding_idx=0)
-        # self.deprel_embedding = nn.Embedding(len(args.tokenizer.vocab['deprel']), args.dep_dim, padding_idx=0)
-        # ''' other parameter '''
-        # self.pad_word = nn.Parameter(torch.zeros(args.hidden_dim * 2), requires_grad=False)
-        # self.pad_edge = nn.Parameter(torch.ones(1), requires_grad=False)
-        # self.ext_rel = nn.Parameter(torch.Tensor(args.dep_dim), requires_grad=True)
-        # ''' main layer '''
-        # self.rnn = WordEmbedLayer(args.word_dim + args.pos_dim, args.hidden_dim, num_layers=1, batch_first=True,
-        #                           bidirectional=True, rnn_type='GRU')  # bi-gru layer
-        self.word_embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1], padding_idx=0)
-        self.word_embedding.weight.data.copy_(torch.from_numpy(embedding_matrix).float())
-        self.word_embedding.weight.requires_grad = False
+        self.word_embedding = nn.Embedding.from_pretrained(torch.tensor(embedding_matrix, dtype=torch.float))
+        self.pos_embedding = nn.Embedding(len(args.tokenizer.vocab['pos']), args.pos_dim, padding_idx=0)
+        self.deprel_embedding = nn.Embedding(len(args.tokenizer.vocab['deprel']), args.dep_dim, padding_idx=0)
+        ''' other parameter '''
+        self.pad_word = nn.Parameter(torch.zeros(args.hidden_dim * 2), requires_grad=False)
+        self.pad_edge = nn.Parameter(torch.ones(1), requires_grad=False)
+        self.ext_rel = nn.Parameter(torch.Tensor(args.dep_dim), requires_grad=True)
+        ''' main layer '''
+        self.rnn = WordEmbedLayer(args.word_dim + args.pos_dim, args.hidden_dim, num_layers=1, batch_first=True,
+                                  bidirectional=True, rnn_type='GRU')  # bi-gru layer
+        # self.word_embedding = nn.Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1], padding_idx=0)
+        # self.word_embedding.weight.data.copy_(torch.from_numpy(embedding_matrix).float())
+        # self.word_embedding.weight.requires_grad = False
         self.cpt = CPT(args, args.word_dim)
         self.linear = nn.Linear(2 * args.hidden_dim, 1)
 
-        # self.bilinear = nn.Bilinear(args.hidden_dim * 4, args.dep_dim, 1)
+        self.bilinear = nn.Bilinear(args.hidden_dim * 4, args.dep_dim, 1)
         self.fc_out = nn.Linear(args.hidden_dim * 2, 3)
         ''' dropout layer '''
-        # self.embed_dropout = nn.Dropout(args.embed_dropout)
-        # self.bilinear_dropout = nn.Dropout(args.bilinear_dropout)
+        self.embed_dropout = nn.Dropout(args.embed_dropout)
+        self.bilinear_dropout = nn.Dropout(args.bilinear_dropout)
         self.fc_dropout = nn.Dropout(args.fc_dropout)
 
     def forward(self, inputs, compressed_version):
         text, pos, deprel, aspect_head, aspect_mask, gather_idx, path, words_ids, aspect_ids, position_weight = inputs
         '''Generate hidden representation for nodes as BiGRU(node_embeddings <+> pos-tag_embeddings)'''
-        # word_feature = self.embed_dropout(self.word_embedding(text))
-        # pos_feature = self.embed_dropout(self.pos_embedding(pos))
-        # text_len = torch.sum(text != 0, dim=-1)
-        #
-        # node_feature, _ = self.rnn(torch.cat((word_feature, pos_feature), dim=-1), text_len.cpu())
-        # BS, SL, FD = node_feature.shape
+        word_feature = self.embed_dropout(self.word_embedding(text))
+        pos_feature = self.embed_dropout(self.pos_embedding(pos))
+        text_len = torch.sum(text != 0, dim=-1)
+
+        node_feature, _ = self.rnn(torch.cat((word_feature, pos_feature), dim=-1), text_len.cpu())
+        BS, SL, FD = node_feature.shape
 
         aspect_feature = self.word_embedding(aspect_ids)
         aspect_lens = torch.sum(aspect_ids != 0, dim=-1)
@@ -113,67 +113,67 @@ class RepWalk(nn.Module):
         v = self.cpt(word_feature1, sent_lens, aspect_feature, aspect_lens, masks, target_masks, position_weight)
 
         '''add a padding word.. somehow this improves performance'''
-        # padword_feature = self.pad_word.reshape(1, 1, -1).expand(BS, -1, -1)
-        # padded_node_feature = torch.cat((padword_feature, node_feature), dim=1)
-        #
-        # '''Gather right words from sentence(by removing embeds of some token words like <p>, based on gather_idx'''
-        # gather_idx = gather_idx.unsqueeze(0).expand(FD, -1, -1).permute(1, 2, 0)
-        # node_feature = torch.cat((padword_feature, node_feature), dim=1)
-        # node_feature = torch.gather(padded_node_feature, 1, gather_idx)
-        #
-        # '''Along with this node feature, concatenate features of head nodes and tail nodes'''
-        # aspect_head = aspect_head.unsqueeze(0).expand(FD, -1, -1).permute(1, 2, 0)
-        # deptext_feature = torch.gather(padded_node_feature, 1, aspect_head)
-        # if compressed_version == False:
-        #     head_text_feature = torch.cat((deptext_feature, node_feature), dim=1)
-        #     exttext_feature = self.pad_word.reshape(1, 1, -1).expand(BS, SL, -1)
-        #     tail_text_feature = torch.cat((node_feature, exttext_feature), dim=1)
-        #     edge_feature = torch.cat((head_text_feature, tail_text_feature), dim=-1)
-        # else:
-        #     edge_feature = torch.cat((deptext_feature, node_feature), dim=-1)
-        #
-        # ''' score function by attention over the node features and edge features '''
-        # if compressed_version == False:
-        #     deprel_feature = self.embed_dropout(self.deprel_embedding(deprel))
-        #     extrel_feature = self.embed_dropout(self.ext_rel).reshape(1, 1, -1).expand(BS, SL, -1)
-        #     label_feature = torch.cat((deprel_feature, extrel_feature), dim=1)
-        # else:
-        #     label_feature = self.embed_dropout(self.deprel_embedding(deprel))
-        # edge_score = torch.sigmoid(self.bilinear(self.bilinear_dropout(edge_feature), label_feature))
-        # padedge_feature = self.pad_edge.reshape(1, 1, -1).expand(BS, -1, -1)
-        # edge_score = torch.cat((padedge_feature, edge_score.transpose(1, 2)), dim=-1).expand(-1, SL, -1)
-        #
-        # ''' node weight '''
-        # if compressed_version == True:
-        #     path = path * (path <= SL)
-        # x = torch.gather(edge_score, 2, path)
-        # node_weight = torch.prod(x, dim=-1, keepdim=True)
-        # text_mask = (text != 0).unsqueeze(-1)
-        # node_weight = torch.where(text_mask != 0, node_weight, torch.zeros_like(node_weight))
-        # aspect_mask = aspect_mask.unsqueeze(-1)
-        # node_weight = torch.where(aspect_mask == 0, node_weight, torch.zeros_like(node_weight))
-        #
-        # # softmax = torch.nn.Softmax(dim=1)
-        # # node_weight = softmax(torch.where(node_weight!=0, node_weight, torch.ones_like(node_weight)*-100000000000))
-        # # node_weight = torch.where(aspect_mask==0, node_weight, torch.zeros_like(node_weight))
-        # # node_weight = node_weight.squeeze(-1)
-        #
+        padword_feature = self.pad_word.reshape(1, 1, -1).expand(BS, -1, -1)
+        padded_node_feature = torch.cat((padword_feature, node_feature), dim=1)
+
+        '''Gather right words from sentence(by removing embeds of some token words like <p>, based on gather_idx'''
+        gather_idx = gather_idx.unsqueeze(0).expand(FD, -1, -1).permute(1, 2, 0)
+        node_feature = torch.cat((padword_feature, node_feature), dim=1)
+        node_feature = torch.gather(padded_node_feature, 1, gather_idx)
+
+        '''Along with this node feature, concatenate features of head nodes and tail nodes'''
+        aspect_head = aspect_head.unsqueeze(0).expand(FD, -1, -1).permute(1, 2, 0)
+        deptext_feature = torch.gather(padded_node_feature, 1, aspect_head)
+        if compressed_version == False:
+            head_text_feature = torch.cat((deptext_feature, node_feature), dim=1)
+            exttext_feature = self.pad_word.reshape(1, 1, -1).expand(BS, SL, -1)
+            tail_text_feature = torch.cat((node_feature, exttext_feature), dim=1)
+            edge_feature = torch.cat((head_text_feature, tail_text_feature), dim=-1)
+        else:
+            edge_feature = torch.cat((deptext_feature, node_feature), dim=-1)
+
+        ''' score function by attention over the node features and edge features '''
+        if compressed_version == False:
+            deprel_feature = self.embed_dropout(self.deprel_embedding(deprel))
+            extrel_feature = self.embed_dropout(self.ext_rel).reshape(1, 1, -1).expand(BS, SL, -1)
+            label_feature = torch.cat((deprel_feature, extrel_feature), dim=1)
+        else:
+            label_feature = self.embed_dropout(self.deprel_embedding(deprel))
+        edge_score = torch.sigmoid(self.bilinear(self.bilinear_dropout(edge_feature), label_feature))
+        padedge_feature = self.pad_edge.reshape(1, 1, -1).expand(BS, -1, -1)
+        edge_score = torch.cat((padedge_feature, edge_score.transpose(1, 2)), dim=-1).expand(-1, SL, -1)
+
+        ''' node weight '''
+        if compressed_version == True:
+            path = path * (path <= SL)
+        x = torch.gather(edge_score, 2, path)
+        node_weight = torch.prod(x, dim=-1, keepdim=True)
+        text_mask = (text != 0).unsqueeze(-1)
+        node_weight = torch.where(text_mask != 0, node_weight, torch.zeros_like(node_weight))
+        aspect_mask = aspect_mask.unsqueeze(-1)
+        node_weight = torch.where(aspect_mask == 0, node_weight, torch.zeros_like(node_weight))
+
+        # softmax = torch.nn.Softmax(dim=1)
+        # node_weight = softmax(torch.where(node_weight!=0, node_weight, torch.ones_like(node_weight)*-100000000000))
+        # node_weight = torch.where(aspect_mask==0, node_weight, torch.zeros_like(node_weight))
         # node_weight = node_weight.squeeze(-1)
-        # # print(node_weight.shape)
-        # # print(torch.sum(node_weight, dim=1).expand(node_weight.shape[1], -1).T.shape)
-        # node_weight = torch.div(node_weight, torch.sum(node_weight, dim=1).expand(node_weight.shape[1], -1).T)
-        # # print(node_weight.shape)
-        #
-        # weight_norm = torch.sum(node_weight, dim=-1)
-        # ''' sentence representation '''
-        # sentence_feature = torch.sum(node_weight.unsqueeze(-1) * node_feature, dim=1)
-        #
-        # t = torch.sigmoid(self.linear(v))
+
+        node_weight = node_weight.squeeze(-1)
+        # print(node_weight.shape)
+        # print(torch.sum(node_weight, dim=1).expand(node_weight.shape[1], -1).T.shape)
+        node_weight = torch.div(node_weight, torch.sum(node_weight, dim=1).expand(node_weight.shape[1], -1).T)
+        # print(node_weight.shape)
+
+        weight_norm = torch.sum(node_weight, dim=-1)
+        ''' sentence representation '''
+        sentence_feature = torch.sum(node_weight.unsqueeze(-1) * node_feature, dim=1)
+
+        t = torch.sigmoid(self.linear(v))
         # t = 1
-        # sentence_feature = (1 - t) * sentence_feature + t * v
-        weight_norm = None
+        sentence_feature = (1 - t) * sentence_feature + t * v
+        # weight_norm = None
         # predicts = self.fc_out(self.fc_dropout(v))
-        predicts = self.fc_out(self.fc_dropout(v))
+        predicts = self.fc_out(self.fc_dropout(sentence_feature))
         return [predicts, weight_norm]
 
 
@@ -219,7 +219,7 @@ class CPT(nn.Module):
             aspect_mid = self.dropout(aspect_mid)
             t = torch.sigmoid(self.linear2[i](v))
             v = (1 - t) * aspect_mid + t * v
-            # v = position_weight.unsqueeze(2) * v
+            v = position_weight.unsqueeze(2) * v
 
         target_masks = target_masks.eq(0).unsqueeze(2).repeat(1, 1, e.shape[2])
         # z, (_, _) = self.lstm3(v, feature_lens)
