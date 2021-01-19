6f4256a88ca7346d3452dc8e5b41a389ff1d13f1
Shamnas <shamnastv@gmail.com>
2021-01-19 22:18:21 +0530

    update

--- RepWalk/train_cpt.py
+++ RepWalk/train_cpt.py
@@ -42,7 +42,7 @@ def retrieve_args():
     parser.add_argument('--wt_decay', default=1e-5, type=float)
     parser.add_argument('--embed_dropout', default=0.5, type=float)
     parser.add_argument('--bilinear_dropout', default=0, type=float)
-    parser.add_argument('--fc_dropout', default=0.3, type=float)
+    parser.add_argument('--fc_dropout', default=0, type=float)
     parser.add_argument('--beta', default=0.01, type=float)
     parser.add_argument('--eps', default=0.01, type=float)
     parser.add_argument('--seed', default=0, type=int)
@@ -55,7 +55,8 @@ def retrieve_args():
     args = parser.parse_args()
     args.dataset_file = dataset_files[args.dataset]
     args.seed = args.seed if args.seed else random.randint(0, 4294967295)
-    args.device = torch.device(args.device) if args.device else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    args.device = torch.device(args.device) if args.device else torch.device(
+        'cuda' if torch.cuda.is_available() else 'cpu')
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
@@ -68,17 +69,17 @@ def retrieve_args():
 
 def weight_init(model):
     for name, param in model.named_parameters():
-            if param.requires_grad:
-                if 'embedding' in name: 
-                    weight = torch.nn.init.xavier_uniform_(torch.zeros_like(param)) 
-                    weight[0] = torch.tensor(0, dtype=param.dtype, device=param.device)
-                    setattr(param, 'data', weight)
+        if param.requires_grad:
+            if 'embedding' in name:
+                weight = torch.nn.init.xavier_uniform_(torch.zeros_like(param))
+                weight[0] = torch.tensor(0, dtype=param.dtype, device=param.device)
+                setattr(param, 'data', weight)
+            else:
+                if len(param.shape) > 1:
+                    torch.nn.init.xavier_uniform_(param)
                 else:
-                    if len(param.shape) > 1:
-                        torch.nn.init.xavier_uniform_(param)
-                    else:
-                        stdv = 1. / math.sqrt(param.size(0))
-                        torch.nn.init.uniform_(param, a=-stdv, b=stdv)
+                    stdv = 1. / math.sqrt(param.size(0))
+                    torch.nn.init.uniform_(param, a=-stdv, b=stdv)
 
 
 def run_train(dataloader, model, criterion, optimizer, device, compress):
@@ -99,7 +100,7 @@ def run_train(dataloader, model, criterion, optimizer, device, compress):
         n_train += len(labels)
     return train_loss / n_train, n_correct / n_train
 
-    
+
 def run_test(dataloader, model, criterion, device, compress):
     test_loss, n_correct, n_test = 0, 0, 0
     labels_all, predicts_all = None, None
@@ -110,26 +111,29 @@ def run_test(dataloader, model, criterion, device, compress):
             labels = sample_batched[1].to(device)
             outputs = model(inputs, compress)
             loss = criterion(outputs[0], labels)
-            
+
             test_loss += loss.item() * len(labels)
             n_correct += (torch.argmax(outputs[0], -1) == labels).sum().item()
             n_test += len(labels)
             labels_all = torch.cat((labels_all, labels), dim=0) if labels_all is not None else labels
             predicts_all = torch.cat((predicts_all, outputs[0]), dim=0) if predicts_all is not None else outputs[0]
-    f1 = metrics.f1_score(labels_all.cpu(), torch.argmax(predicts_all, -1).cpu(), labels=[0, 1, 2], average='macro') # compute f1 score
+    f1 = metrics.f1_score(labels_all.cpu(), torch.argmax(predicts_all, -1).cpu(), labels=[0, 1, 2],
+                          average='macro')  # compute f1 score
     return test_loss / n_test, n_correct / n_test, f1
 
 
 def main():
     args = retrieve_args()
-    args.tokenizer = build_tokenizer(fnames=args.dataset_file.values(), dataset=args.dataset) # transfrom tokens to indices
+    args.tokenizer = build_tokenizer(fnames=args.dataset_file.values(),
+                                     dataset=args.dataset)  # transfrom tokens to indices
     print(args.tokenizer.max_slen)
     print(args.tokenizer.max_tlen)
     print(args.tokenizer.max_plen)
 
-    embedding_matrix = build_embedding_matrix(vocab=args.tokenizer.vocab['word'], dataset=args.dataset, dir_path = '../..') # pre-trained glove embeddings
-    trainset = MyDataset(fname=args.dataset_file['train'], tokenizer=args.tokenizer) #TODO
-    testset = MyDataset(fname=args.dataset_file['test'], tokenizer=args.tokenizer) #TODO
+    embedding_matrix = build_embedding_matrix(vocab=args.tokenizer.vocab['word'], dataset=args.dataset,
+                                              dir_path='../..')  # pre-trained glove embeddings
+    trainset = MyDataset(fname=args.dataset_file['train'], tokenizer=args.tokenizer)  # TODO
+    testset = MyDataset(fname=args.dataset_file['test'], tokenizer=args.tokenizer)  # TODO
     model = RepWalk(embedding_matrix, args).to(args.device)
 
     _params = filter(lambda p: p.requires_grad, model.parameters())
@@ -163,7 +167,8 @@ def main():
             best_epoch = epoch
         else:
             scheduler.step()
-        print(f"{100*(epoch+1)/args.num_epoch:6.2f}% > loss: {train_loss:.4f}, acc: {train_acc:.4f}, test acc: {test_acc:.4f}, test f1: {test_f1:.4f}")
+        print(
+            f"{100 * (epoch + 1) / args.num_epoch:6.2f}% > loss: {train_loss:.4f}, acc: {train_acc:.4f}, test acc: {test_acc:.4f}, test f1: {test_f1:.4f}")
         print('', flush=True)
         if epoch > best_epoch + 10:
             break
