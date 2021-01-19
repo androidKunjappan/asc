4c8ee8d8e9abc6185bf53faa5aa3980ac5ef04ac
Shamnas <shamnastv@gmail.com>
2021-01-19 17:02:44 +0530

    update

--- pss/twitter.sh
+++ pss/twitter.sh
@@ -6,4 +6,4 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --hidden_dim 50 --lr .001 --batch_size 64 --dataset_name Twitter --beta .5
+python3 main.py --hidden_dim 50 --lr .001 --batch_size 64 --dataset_name Twitter --beta .5 --weight_decay 0
