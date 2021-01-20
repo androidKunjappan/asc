7e3ef71e44d434107579d46b05c907eafc48c4e4
Shamnas <shamnastv@gmail.com>
2021-01-20 20:25:23 +0530

    update

--- RepWalk_combined/twitter.sh
+++ RepWalk_combined/twitter.sh
@@ -6,4 +6,4 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl2_48h-1G
 
-python3 train.py --cpt --dataset twitter --lr .001 --batch_size 64 --phi 10.0 --entropy 2.0 --eps 0.01 --beta 0.01 --no_itr
+python3 train.py --cpt --dataset twitter --lr .001 --batch_size 100 --phi 10.0 --entropy 2.0 --eps 0.01 --beta 0.01 --no_itr --num_epoch 40
